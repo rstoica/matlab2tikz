e19f74a7ddc6556f6c0706f20c9069755484f5aa
Andrei Stoica <stoica.razvan.andrei@gmail.com>
2018-03-25 22:50:08 +0200

    Added latex math string formatting by default

--- src/matlab2tikzpdf.m
+++ src/matlab2tikzpdf.m
@@ -7,8 +7,6 @@ function matlab2tikzpdf(varargin)
     %   utilities (for now I only care about Linux / Unix systems)
     %SEE ALSO: MATLAB2TIKZ
     
-    
-    % call MATLAB2TIKZ
     if (nargin == 0)
         error('Please provide at least the name of the tex file to contain the TikZ code');
     elseif (nargin == 1)
@@ -17,11 +15,18 @@ function matlab2tikzpdf(varargin)
             varargin{2} = varargin{1};
             varargin{1} = 'filename';
         else
+            warning('Invalid filename %s! Expected latex of tikz formats.', varargin{1});
             varargin{2} = 'output.tex';
             varargin{1} = 'filename';
         end
     end
     
+    % We want latex math formatting
+    varargin{length(varargin)+1} = 'parseStringsAsMath';
+    varargin{length(varargin)+1} = true;
+    varargin{length(varargin)+1} = 'parseStrings';
+    varargin{length(varargin)+1} = false;
+    % call MATLAB2TIKZ to produce main tikz/tex figure
     matlab2tikz(varargin{:});
 
     % Create pdflatex file
@@ -37,23 +42,19 @@ function matlab2tikzpdf(varargin)
                        '\\usepgfplotslibrary{patchplots}\n', ...
                        '\\usepackage{grffile}\n', ...
                        '\\usepackage{amsmath}\n', ...
+                       '\\pagestyle{empty}\n', ...
                        '\\begin{document}\n', ...
                        '    \\input{',varargin{2},'}\n', ...
                        '\\end{document}\n']);
     fprintf(fid, '%s', textext);
     fclose(fid);
     
-    % Call system utilities on tex file
-    if (strcmp(ext,'.tex'))
-        % Store the filename to process
-        [~, basename, ~] = fileparts(texfile);    
-        fileouttex = [basename, '.pdf'];
-        filecrop = [basename, '-crop.pdf'];
-        [~, basename, ~] = fileparts(varargin{2});
-        fileout = [basename,'.pdf'];
-        system(['pdflatex ',texfile,' && pdfcrop ',fileouttex,' && mv ',filecrop,' ',fileout,' && rm ',fileouttex],'-echo');
-        system(['rm ',logtexfile,' ',auxtexfile]);
-    else
-        warning('Cannot convert .tikz files to PDFs! These can be imported manually.');
-    end
+    % Store the filename to process
+    [~, basename, ~] = fileparts(texfile);    
+    fileouttex = [basename, '.pdf'];
+    filecrop = [basename, '-crop.pdf'];
+    [~, basename, ~] = fileparts(varargin{2});
+    fileout = [basename,'.pdf'];
+    system(['pdflatex ',texfile,' && pdfcrop ',fileouttex,' && mv ',filecrop,' ',fileout,' && rm ',fileouttex],'-echo');
+    system(['rm ',logtexfile,' ',auxtexfile]);
 end
